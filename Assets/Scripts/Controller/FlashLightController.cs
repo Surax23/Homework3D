c5f216d9d5d6294e1eca45b4b77f83317bc642bc
Александр Сурков <rm2kfiles@ya.ru>
2019-02-01 21:19:43 +0300

    Initial commit.

---
+++ Assets/Scripts/Controller/FlashLightController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+	public class FlashLightController : BaseController
+	{
+		private FlashLightModel _flashLight;
+		private FlashLightUiText _flashLightUi;
+
+		public FlashLightController()
+		{
+			_flashLight = MonoBehaviour.FindObjectOfType<FlashLightModel>();
+			_flashLightUi = MonoBehaviour.FindObjectOfType<FlashLightUiText>();
+			Off();
+		}
+
+		public override void OnUpdate()
+		{
+			if (!IsActive) return;
+
+			if (_flashLight == null)return;
+			_flashLight.Rotation();
+			if (_flashLight.EditBatteryCharge())
+			{
+				_flashLightUi.Text = _flashLight.BatteryChargeCurrent;
+			}
+			else
+			{
+				Off();
+			}
+		}
+
+		public override void On()
+		{
+			if (IsActive)return;
+			base.On();
+			_flashLight.Switch(true);
+			_flashLightUi.SetActive(true);
+		}
+
+		public sealed override void Off()
+		{
+			if (!IsActive) return;
+			base.Off();
+			_flashLight.Switch(false);
+			_flashLightUi.SetActive(false);
+		}
+	}
+}
