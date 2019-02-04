7062c6d0a42078ab21d17dedf3bb6976de763825
Александр Сурков <rm2kfiles@ya.ru>
2019-02-05 02:45:10 +0300

    a_try_to_add_coloring
    Попытка сделать раскрашивание объектов при наведении и щелчке правой кнопкой мыши, но что-то пошло не так и оно не работает. Подскажите, пожалуйста, куда копать.

---
+++ Assets/Scripts/Controller/RayCastController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+    public sealed class RayCastController : BaseController
+    {
+        private RayCastModel _rayCastModel;
+        private BaseObjectScene temp_obj;
+
+        public RayCastController()
+        {
+            Debug.Log("I'm trying to work: finding!");
+            _rayCastModel = MonoBehaviour.FindObjectOfType<RayCastModel>();
+            //Debug.Log(_rayCastModel.GetInstanceID());
+        }
+
+        public override void OnUpdate()
+        {
+            if (!IsActive) return;
+
+            temp_obj = _rayCastModel.SelectedObj();
+            if (temp_obj != null)
+            {
+                Debug.Log("I'm trying to work: changing color!");
+                temp_obj.Color = Color.red;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/RayCastModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Geekbrains
+{
+    class RayCastModel : BaseObjectScene
+    {
+        private Ray ray;
+        private RaycastHit hit;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            Debug.Log("I'm trying to work: creating ray!");
+        }
+
+        public BaseObjectScene SelectedObj()
+        {
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 50f, Color.red);
+            Debug.Log("I'm trying to work: trying to hit an object!");
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Debug.Log("I'm trying to work: hitting object!");
+                return hit.transform.gameObject.GetComponent<BaseObjectScene>();
+            }
+            return null;
+        }
+    }
+}
